25b4710c376770bb9ae6bb47f33d3d88ae31c075
Paula C Schuler <paulasatx@gmail.com>
2015-03-19 17:10:24 -0500

    My_Favorite_Things.php server_name_generator.php complete.

--- public/my_favorite_things.php
+++ public/my_favorite_things.php
@@ -1,23 +1,55 @@
 
-<?php $myFaves = ["Dardago", "Photos", "Writings", "The Outdoors", "Valentine", "Glasses", "Plane Tickets", "The Stage"];   ?>
-<?php function for 888888 NOT DONE, ADD THE LOOP   ?>
-<!DOCTYPE html>
+
+<? 
+function pageController()
+{
+    // Initialize an empty data array.
+    $arrayFaves = [];
+    
+
+    // Add data to be used in the html view.
+    $arrayFaves['favorites'] = ["Polcinego", "Plane Tickets", "Photos", "Writing", "Cathedral Acoustics"];
+
+    // Return the completed data array.
+    return $arrayFaves;    
+}
+
+// Call the pageController function and extract all the returned array as local variables.
+extract(pageController());
+
+?>
+
+<!DOCTYPE html> 
 <html>
 <head>
-	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 	<style type="text/css">
 	body {
 		background-color: #AF92BA;
 	}
+	h1 {
+		text-align: center;
+	}
+	table {
+		text-align: center;
+	}
+	tr {
+		text-align: center;
+	}
 	</style>
 	<table></table>
 	<title>my_favorites</title>
 </head>
 <body>
-	<table class="table table-striped"><?php    ?>
-
+	<h1>My Favorites</h1>
+	<table class="table table-striped">        
+		<? foreach ($favorites as $fave): ?>
+		<tr>	 
+			<td><?= $fave ?></td>
+		</tr>
+		<? endforeach ?>
+				
 	</table>
 
 
--- public/server_name_generator.php
+++ public/server_name_generator.php
@@ -1,16 +1,27 @@
-<?php $nouns = ["Cello", "Bass", "Guitar", "Troubador", "Singer", "Stage", "Conductor", "Drummer", "Microphone", "Fans"]; ?>
-<?php $nounsRandom = rand(0, 9); ?>
-<?php $adjectives = ["Boiling", "Salty", "Sweet", "Hot", "Flat", "Comfort", "Lumpy", "Melty", "Chewy", "Sticky"]; ?>
-<?php $adjectivesRandom = rand(0, 9)?>
+<?
 
+function pageController()
+{
+	$data = [];
+	$data['nouns'] = ["Cello", "Bass", "Guitar", "Troubador", "Singer", "Stage", "Conductor", "Drummer", "Microphone", "Fans"];
+	$data["nounsRandom"] = array_rand($data['nouns']); 
+	echo $data['nounsRandom'];
+	$data['adjectives'] = ["Boiling", "Salty", "Sweet", "Hot", "Flat", "Comfort", "Lumpy", "Melty", "Chewy", "Sticky"];
+	$data["adjectivesRandom"] = array_rand($data['adjectives']);
+	return $data;
+}
+extract(pageController());
+?>
 <!DOCTYPE html>
 <html>
 <head>
+	<link href='http://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
 	<style type="text/css">
 		body {
 			background-color: #603E11;
 			width: 100%;
 			height: 100%;
+			font-family: 'Indie Flower', cursive;
 		}
 		div {
 			background-color: #ECE8DB;
@@ -25,7 +36,7 @@
 </head>
 <body>
 	<div>
-		<h1>Your Random Server Name Is: <?php echo "$adjectives[$adjectivesRandom]" . " " . "$nouns[$nounsRandom]" . PHP_EOL;?></h1>
+		<h1>Your Random Server Name Is:   <?= $adjectives[$adjectivesRandom] . " " . $nouns[$nounsRandom] . PHP_EOL;?></h1>
 	</div>
 </body>
 </html>
