e5db53d6b379f8a9e13819e2109fc7d02836586d
Paula C Schuler <paulasatx@gmail.com>
2015-03-19 11:39:11 -0500

    my_favorite_things.php first commit.

---
+++ public/my_favorite_things.php
@@ -0,0 +1,25 @@
+
+<?php $myFaves = ["Dardago", "Photos", "Writings", "The Outdoors", "Valentine", "Glasses", "Plane Tickets", "The Stage"];   ?>
+<?php function for 888888 NOT DONE, ADD THE LOOP   ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	<style type="text/css">
+	body {
+		background-color: #AF92BA;
+	}
+	</style>
+	<table></table>
+	<title>my_favorites</title>
+</head>
+<body>
+	<table class="table table-striped"><?php    ?>
+
+	</table>
+
+
+</body>
+</html>
