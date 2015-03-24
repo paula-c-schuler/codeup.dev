1b5c55c8b78c4c6d006495347a7e33e640394219
Paula C Schuler <paulasatx@gmail.com>
2015-03-24 12:10:56 -0500

    Added header.php to the Require PHP exercise for templating a site. Only the most basic functionality so far, but it works.

--- public/body.php
+++
@@ -1,6 +0,0 @@
-<!--  EXERCISE 7.2 COMPLETE IN MOST BASIC FORM, COULD USE MORE PRETTY AND DETAIL  -->
-<div class="container">
-	<div>
-		<h1>This is the body of this page.</h1>  
-	</div>
-</div>
--- public/index_required_include.php
+++ public/index_required_include.php
@@ -1,30 +1,9 @@
+<!DOCTYPE html>
 <html>
 <!-- ******   EXERCISE 7.2 WORKING IN MOST BASIC FORM  ****** -->
-<head>
-	<title></title>
-	<style type="text/css">
-	.navbar {
-		background-color: #AF92BA;
-	}
-	body {
-		height: 300px;
-		background-color: #E6D9EB;
-	}
-	.footer {
-		background-color: #AF92BA;
-		height: 50px;
-	}
-		</style>
-	<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-</head>
+<?php require 'header.php';?>
 <body>
 	<?php require 'navbar.php';?>
-
-	<?php require 'body.php';?>
 
 	<?php require 'footer.php';?>
 </body>
