de939051142a7f3aabad8ff589360b2e1539a38f
Paula C Schuler <paulasatx@gmail.com>
2015-04-02 15:56:33 -0500

    CSS exercises, PHP IV started.

---
+++ park_migrations.php
@@ -0,0 +1,17 @@
+<?php 
+
+define('DB_HOST','127.0.0.1');
+
+define('DB_NAME','parks_db');
+
+define('DB_USER','parks_user'); 
+
+define('DB_PASS','freefree');
+
+REQUIRE ('db_connect.php'); 
+
+echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
+
+
+
+ ?>
---
+++ pdo_test.php
@@ -0,0 +1,15 @@
+<?php 
+
+define('DB_HOST','127.0.0.1');
+
+define('DB_NAME','employees');
+
+define('DB_USER','codeup'); 
+
+define('DB_PASS','pcspcs');
+
+REQUIRE ('db_connect.php'); 
+
+echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
+
+ ?>
