ed201d5d0c1a07af1e88592c43d8b606fe8536f6
Paula C Schuler <paulasatx@gmail.com>
2015-04-07 16:01:26 -0500

    Laid out the table and identified classes.
    Needed to do this for formatting and UX.

--- public/parks.php
+++ public/parks.php
@@ -9,26 +9,27 @@ $parksReturn = $dbc->query('SELECT * FROM parks')->fetchAll(PDO::FETCH_ASSOC);
  ?>
 <html>
 <head>
+	<style type="text/css" src='/css/parks.css'></style>
 	<title>National Parks</title>
 </head>
 	<body>
 		<table>
 			<thead>
 				<tr>
-					<th>Location</th>
-					<th>Name</th>
-					<th>Date Established</th>
-					<th>Area in Acres</th>
+					<th class='column1' 'th'>Location</th>
+					<th class='column2' 'th'>Name</th>
+					<th class='column3' 'th'>Date Established</th>
+					<th class='column4' 'th'>Area in Acres</th>
 				</tr>
 			</thead>
+				<?php foreach ($parksReturn as $park): ?>
 				<tr>
-					<?php     
-					foreach ($parksReturn as $park) {
-						echo $park['location'];
-					} 
-					?>
+					<td class='column1' 'td'><?= $park['location']; ?></td>	
+					<td class='column2' 'td'><?= $park['name']; ?></td>
+					<td class='column3' 'td'><?= $park['date_established']; ?></td>
+					<td class='column4' 'td'><?= $park['area_in_acres']; ?></td>
 				</tr>
-			
+			<?php endforeach; ?>
 		</table>
 
 	</body>
