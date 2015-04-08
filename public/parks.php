a6710d451e8907717748fccbf2b6bb07ef4c2532
Paula C Schuler <paulasatx@gmail.com>
2015-04-08 13:30:27 -0500

    Assignment complete 1-3, exercise 9.1.3 PDO
    Updating, to make sure I can retrieve.

--- public/parks.php
+++ public/parks.php
@@ -3,13 +3,17 @@
 require '../parks_login.php';
 require '../db_connect.php';
 
+// get page
+$userPage = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+
+// define variable for how many allowed per page, up to limit of 10
+$perPage = isset($_GET['perPage']) ? (int)$_GET['perPage'] : 4;
+
+// offset variable created using variables defined above in ternery statement 
+$start = ($userPage > 1) ? ($userPage * $perPage) - $perPage : 0;
 
 // $stmt = $dbc->query('SELECT * FROM parks');
-$parksReturn = $dbc->query('SELECT * FROM parks LIMIT 4')->fetchAll(PDO::FETCH_ASSOC);
-
-
-
-
+$parksReturn = $dbc->query('SELECT * FROM parks LIMIT ' . $perPage . ' OFFSET ' . $start)->fetchAll(PDO::FETCH_ASSOC);
 
 
 
@@ -34,17 +38,19 @@ $parksReturn = $dbc->query('SELECT * FROM parks LIMIT 4')->fetchAll(PDO::FETCH_A
 					<th class='column4' 'th'>Area in Acres</th>
 				</tr>
 			</thead>
-				<?php foreach ($parksReturn as $park): ?>
+				<!-- <?php foreach ($parksReturn as $park): ?> -->
 				<tr>
 					<td class='column1' 'td'><?= $park['location']; ?></td>	
 					<td class='column2' 'td'><?= $park['name']; ?></td>
 					<td class='column3' 'td'><?= $park['date_established']; ?></td>
 					<td class='column4' 'td'><?= $park['area_in_acres']; ?></td>
 				</tr>
-			<?php endforeach; ?>
+			<!-- <?php endforeach; ?> -->
 		</table>
-		<button id="next">  >  </button>
-		<button id="back">  <  </button>
+		<!-- <button><a href="http://codeup.dev/parks.php"></a> Page 1 </button> -->
+		<a id="next" href="http://codeup.dev/parks.php?perpage=4&page=2"> Next </a>
+		<a id="previous" href="http://codeup.dev/parks.php?perpage=4&page=2"> Previous </a>
+		
 	</body>
 </html>
 
