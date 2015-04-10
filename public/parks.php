721480423fc3e748efcb60f86989a7a3c5580701
Paula C Schuler <paulasatx@gmail.com>
2015-04-09 20:46:38 -0500

    Exerc 9.1.3 Prepared statement PDO, LIMIT, OFFSET
    Documenting all error messages are resolved for the first time.
    It is assumed, so far, that LIMIT and OFFSET work properly.
    No code is commented out, including the population of the table in HTML.
    No unidentified variable messages, etcetera.
    Further testing may reveal I am mistaken, however.

--- public/parks.php
+++ public/parks.php
@@ -1,30 +1,30 @@
 <?php  
-
 require '../parks_login.php';
 require '../db_connect.php';
 
+// get current page
 $userPage = isset($_GET['page']) ? (int)$_GET['page'] : 1;
 
-// define variable for how many allowed per page, up to limit of 10
+// limit number of results per page
 $perPage = isset($_GET['perPage']) ? (int)$_GET['perPage'] : 4;
-// var_dump($perPage);
 
-// offset variable created using variables defined above in ternery statement 
+// determine next set of results based on current page
 $start = ($userPage > 1) ? ($userPage * $perPage) - $perPage : 0;
-
 $query = 'SELECT * FROM parks LIMIT :limit OFFSET :offset';
 
+// prepare statement
+// bindValue added to ensure integer data type
 $stmt = $dbc->prepare($query);
 $stmt->bindValue(':limit', $perPage, PDO::PARAM_INT);
 $stmt->bindValue(':offset', $start, PDO::PARAM_INT);
 $stmt->execute();
+print_r($stmt->fetch(PDO::FETCH_ASSOC));
 
-// print_r($stmt->fetch(PDO::FETCH_ASSOC));
-
-$stmt = $dbc->prepare('INSERT INTO parks (location, name, date_established, area_in_acres, description) 
-		VALUES (:location, :name, :date_established, :area_in_acres, :description)');
-
-
+$parks = [];
+$stmt = $dbc->prepare('INSERT INTO parks (location, name, 
+	date_established, area_in_acres, description) 
+	VALUES (:location, :name, :date_established, :area_in_acres, 
+	:description)');
 echo "Inserted ID: " . $dbc->lastInsertId() . PHP_EOL;
 
 
@@ -50,15 +50,15 @@ echo "Inserted ID: " . $dbc->lastInsertId() . PHP_EOL;
 					<th class='column5' 'th'>Description</th>
 				</tr>
 			</thead>
-				<!-- <?php foreach ($parksReturn as $park): ?> -->
+				<?php foreach ($parks as $park): ?>
 				<tr>
-					<td class='column1' 'td'><?= $park[':location']; ?></td>	
-					<td class='column2' 'td'><?= $park[':name']; ?></td>
+					<td class='column1' 'td'><?= ':location'; ?></td>	
+					<td class='column2' 'td'><?= ':name'; ?></td>
 					<td class='column3' 'td'><?= $park[':date_established']; ?></td>
 					<td class='column4' 'td'><?= $park[':area_in_acres']; ?></td>
 					<td class='column5' 'td'><?= $park[':description']; ?></td>
 				</tr>
-			<!-- <?php endforeach; ?> -->
+				<?php endforeach; ?>
 		</table>
 		<!-- <button><a href="http://codeup.dev/parks.php"></a> Page 1 </button> -->
 
